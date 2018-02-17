e5c07aed4be95936f29412c6ed54c37b9957fcfb
Loenan <laurent.istin@free.fr>
2018-02-17 02:15:22 +0100

    Add sphere parts

---
+++ src/main/java/com/loenan/bricks/ldraw/model/Parts.java
@@ -0,0 +1,10 @@
+package com.loenan.bricks.ldraw.model;
+
+public interface Parts {
+
+    NamedPart TILE_1X1 = new NamedPart("3070b");
+
+    Part SLOPE_31_1X1 = new Part(54200, "Slope Brick 31 1 x 1 x 0.667");
+
+    Part SLOPE_PLATE_45_2X1 = new Part(15672, "Slope Plate 45 2x1");
+}
