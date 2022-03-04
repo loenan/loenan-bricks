8c7410d8e0a8880555eef0afc40ebf157516707c
loenan <laurent.istin@free.fr>
2022-03-04 11:19:10 +0100

    fix: Matrix.combine() was returning the transpose of the result

--- brick-sphere-generator/src/main/java/com/loenan/bricks/sphere/generator/geometry/CubeFace.java
+++ brick-sphere-generator/src/main/java/com/loenan/bricks/sphere/generator/geometry/CubeFace.java
@@ -7,17 +7,17 @@ import static com.loenan.bricks.ldraw.geometry.Angle.ANTI_QUARTER_TURN;
 import static com.loenan.bricks.ldraw.geometry.Angle.HALF_TURN;
 import static com.loenan.bricks.ldraw.geometry.Angle.QUARTER_TURN;
 import static com.loenan.bricks.ldraw.geometry.Transformation.IDENTITY;
-import static com.loenan.bricks.ldraw.geometry.Transformation.rotationX;
+import static com.loenan.bricks.ldraw.geometry.Transformation.rotationY;
 import static com.loenan.bricks.ldraw.geometry.Transformation.rotationZ;
 
 public enum CubeFace {
 
 	TOP(0, -1, 0, IDENTITY),
 	BOTTOM(0, 1, 0, rotationZ(HALF_TURN)),
-	FRONT(1, 0, 0, rotationZ(ANTI_QUARTER_TURN).combine(rotationX(QUARTER_TURN))),
-	BACK(-1, 0, 0, rotationZ(QUARTER_TURN).combine(rotationX(QUARTER_TURN))),
-	LEFT(0, 0, 1, rotationX(QUARTER_TURN).combine(rotationZ(ANTI_QUARTER_TURN))),
-	RIGHT(0, 0, -1, rotationX(ANTI_QUARTER_TURN).combine(rotationZ(ANTI_QUARTER_TURN)));
+	FRONT(1, 0, 0, rotationZ(QUARTER_TURN).combine(rotationY(QUARTER_TURN))),
+	BACK(-1, 0, 0, rotationZ(ANTI_QUARTER_TURN).combine(rotationY(ANTI_QUARTER_TURN))),
+	LEFT(0, 0, 1, rotationY(ANTI_QUARTER_TURN).combine(rotationZ(QUARTER_TURN))),
+	RIGHT(0, 0, -1, rotationY(QUARTER_TURN).combine(rotationZ(QUARTER_TURN)));
 
 	private final Vector translation;
 	private final Matrix transformation;
--- ldraw-builder/src/main/java/com/loenan/bricks/ldraw/geometry/Matrix.java
+++ ldraw-builder/src/main/java/com/loenan/bricks/ldraw/geometry/Matrix.java
@@ -25,10 +25,9 @@ public class Matrix {
 			.orElseThrow(() -> new NoSuchElementException("No matrix defined"));
 	}
 
-	public Matrix(
-			double a, double b, double c,
-			double d, double e, double f,
-			double g, double h, double i) {
+	public Matrix(double a, double b, double c,
+				  double d, double e, double f,
+				  double g, double h, double i) {
 		this.a = a;
 		this.b = b;
 		this.c = c;
@@ -78,22 +77,24 @@ public class Matrix {
 
 	public Vector transform(Vector v) {
 		return new Vector(
-				a * v.getX() + b * v.getY() + c * v.getZ(),
-				d * v.getX() + e * v.getY() + f * v.getZ(),
-				g * v.getX() + h * v.getY() + i * v.getZ());
+			a * v.getX() + b * v.getY() + c * v.getZ(),
+			d * v.getX() + e * v.getY() + f * v.getZ(),
+			g * v.getX() + h * v.getY() + i * v.getZ());
 	}
 
 	public Matrix combine(Matrix m) {
 		return new Matrix(
-				m.a * a + m.d * b + m.g * c,
-				m.a * d + m.d * e + m.g * f,
-				m.a * g + m.d * h + m.g * i,
-				m.b * a + m.e * b + m.h * c,
-				m.b * d + m.e * e + m.h * f,
-				m.b * g + m.e * h + m.h * i,
-				m.c * a + m.f * b + m.i * c,
-				m.c * d + m.f * e + m.i * f,
-				m.c * g + m.f * h + m.i * i);
+			m.a * a + m.d * b + m.g * c,
+			m.b * a + m.e * b + m.h * c,
+			m.c * a + m.f * b + m.i * c,
+
+			m.a * d + m.d * e + m.g * f,
+			m.b * d + m.e * e + m.h * f,
+			m.c * d + m.f * e + m.i * f,
+
+			m.a * g + m.d * h + m.g * i,
+			m.b * g + m.e * h + m.h * i,
+			m.c * g + m.f * h + m.i * i);
 	}
 
 	public String format() {
